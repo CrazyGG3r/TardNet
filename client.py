107a0893e766fc0404835b62294b2068f77080ff
AliHasnain Ladhani <64027795+alihasnainladhani@users.noreply.github.com>
2024-04-24 12:10:34 +0500

    remove comments

--- client.py
+++ client.py
@@ -1,6 +1,5 @@
 import socket
 
-# Set up client
 HOST = "127.0.0.1"
 PORT = 12345
 
@@ -8,11 +7,8 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
 while True:
-    # Send message to server
     message = input("Enter your message: ")
     client.send(message.encode("utf-8"))
-
-    # Receive response from server
     response = client.recv(1024).decode("utf-8")
-    print("Server:", response)
+    print("ServerSorry:", response)
 
